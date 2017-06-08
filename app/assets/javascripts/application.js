7dad588e4818bf8edbc348b7646f1769754aa7ff
Sagar <=>
2017-06-08 19:24:54 +0000

    Styling for LMS app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,8 +16,20 @@
 //= require turbolinks
 //= require_tree .
 
+
+// js for initialize foundation
 $(function() {
     $(document).foundation();
 });
-
-$(function(){ $(document).foundation(); });
+// js for navigation
+$(function() {
+    $(window).scroll(function() {
+        var winTop = $(window).scrollTop();
+        if (winTop >= 30) {
+            $("body").addClass("sticky-shrinknav-wrapper");
+        }
+        else {
+            $("body").removeClass("sticky-shrinknav-wrapper");
+        }
+    });
+});
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,6 +10,7 @@ class StudentsController < ApplicationController
   # GET /students/1
   # GET /students/1.json
   def show
+    
   end
 
   # GET /students/new
